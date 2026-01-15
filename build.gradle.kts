160b7ba448372de2a5a67c1905063187e9c94cec
Leon Linhart <themrmilchmann@gmail.com>
2026-01-15 20:33:59 +0100

    build: fix SQLite link

--- build.gradle.kts
+++ build.gradle.kts
@@ -91,7 +91,7 @@ android {
 
 tasks {
     val downloadSqlite = register<Download>("downloadSqlite") {
-        src = "https://www.sqlite.org/2025/sqlite-amalgamation-3510200.zip"
+        src = "https://www.sqlite.org/2026/sqlite-amalgamation-3510200.zip"
         dest = layout.buildDirectory.file("tmp/sqlite.zip")
 
         checksum = "9a9dd4eef7a97809bfacd84a7db5080a5c0eff7aaf1fc1aca20a6dc9a0c26f96"
