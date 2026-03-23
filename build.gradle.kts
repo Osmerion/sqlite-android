31c1c2547798572d2ee9a847f00e6cafca4a27ac
Leon Linhart <themrmilchmann@gmail.com>
2026-03-23 18:47:57 +0100

    build: update SQLite {3.52.0 => 3.51.3}

--- build.gradle.kts
+++ build.gradle.kts
@@ -90,10 +90,10 @@ android {
 
 tasks {
     val downloadSqlite = register<Download>("downloadSqlite") {
-        src = "https://www.sqlite.org/2026/sqlite-amalgamation-3520000.zip"
+        src = "https://www.sqlite.org/2026/sqlite-amalgamation-3510300.zip"
         dest = layout.buildDirectory.file("tmp/sqlite.zip")
 
-        checksum = "3fab435106250ea1cdfd5f9fa1657ad832fd431df1d817d48af66ba166e0d16e"
+        checksum = "ced02ff9738970f338c9c8e269897b554bcda73f6cf1029d49459e1324dbeaea"
         hashingAlgorithm = "SHA3-256"
 
         outputs.upToDateWhen { true }
